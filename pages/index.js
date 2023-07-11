c14027a3d6415292fd31a2c6ba22f4083c3638d1
Alex <alexandraolsavsky@gmail.com>
2023-07-11 16:21:16 -0500

    import typo fix

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import {useState} from "react";
-import Head from "next/Head";
+import { useState } from "react";
+import Head from "next/head";
 import Header from "@/components/Header";
 import Intro from "@/components/Intro";
 import Footer from "@/components/Footer";
@@ -17,24 +17,24 @@ export default function Home() {
 				<title>AI Workout Wizard</title>
 			</Head>
 			<div className={'w-full flex-grow'}>
-				<Header/>
+				<Header />
 				<div className={'mt-24 px-4 my-6'}>
 					<div className={'max-w-5xl mx-auto'}>
-						<Intro/>
+						<Intro />
 						<div className={'w-full'}>
-							<UserForm setData={setData} setLoading={setLoading} loading={loading}/>
+							<UserForm setData={setData} setLoading={setLoading} loading={loading} />
 						</div>
 						{
 							loading
 								? <div className={'w-full text-center text-white text-secondary-light p-2 text-xl font-normal'}>Working on it...</div>
 								: data.length > 0
-									? <TableToPDF data={data}/>
+									? <TableToPDF data={data} />
 									: undefined
 						}
 					</div>
 				</div>
 			</div>
-			<Footer/>
+			<Footer />
 		</main>
 	);
 }
