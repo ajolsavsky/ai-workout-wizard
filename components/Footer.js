051d266b7e45ee904291f71eb1e4f610d205ecfa
Alex <alexandraolsavsky@gmail.com>
2023-07-11 13:57:50 -0500

    Pax footer

--- components/Footer.js
+++ components/Footer.js
@@ -6,11 +6,11 @@ export default function Footer() {
 			Created by {' '}
 			<Link
 				className={'text-primary-main'}
-				href={"https://www.mejaz.in"}
+				href={"https://studiopax.io/"}
 				target={'_blank'}
 				rel={'noopener noreferrer'}
 			>
-				Mohd Ejaz Siddiqui
+				Studio Pax
 			</Link>
 		</div>
 	)
