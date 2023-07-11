7061e031af1dfbef452968abe7e719f6fb4fd9d4
Alex <alexandraolsavsky@gmail.com>
2023-07-11 13:52:19 -0500

    Prellim pax customization

--- components/Intro.js
+++ components/Intro.js
@@ -2,11 +2,10 @@ export default function Intro() {
 	return (
 		<div className={'text-md text-zinc-600 mt-3'}>
 			<p className={"font-semibold text-2xl"}>
-				Welcome to our fitness app!
+				Welcome to the PAX Palm API test!
 			</p>
 			<p className={"mt-2"}>
-				Get personalized exercise plans tailored to your needs. Beginner or experienced, we guide you towards a
-				healthier, fitter you. <span className={"mt-2 font-bold"}>Start your fitness journey today!</span>
+				Get started by filling in your desired SOP output.
 			</p>
 		</div>
 	)
--- palm2Services.js
+++ palm2Services.js
@@ -1,5 +1,5 @@
-import {TextServiceClient} from "@google-ai/generativelanguage"
-import {GoogleAuth} from "google-auth-library"
+import { TextServiceClient } from "@google-ai/generativelanguage"
+import { GoogleAuth } from "google-auth-library"
 
 const MODEL_NAME = "models/text-bison-001";
 const API_KEY = process.env.PALM_API_KEY;
@@ -14,7 +14,7 @@ const extractJSON = (output) => {
 	const jsonEndIndex = output.lastIndexOf("]") + 1;
 	const json = output.substring(jsonStartIndex, jsonEndIndex);
 
-	console.log(json)
+	console.log(output);
 
 	// Parse the JSON
 	return JSON.parse(json);
@@ -35,13 +35,13 @@ export const generateText = async (prompt) => {
 		// optional, maximum number of output tokens to generate
 		max_output_tokens: 2000,
 		// optional, safety settings
-		safety_settings: [{"category": "HARM_CATEGORY_DEROGATORY", "threshold": 1}, {
+		safety_settings: [{ "category": "HARM_CATEGORY_DEROGATORY", "threshold": 1 }, {
 			"category": "HARM_CATEGORY_TOXICITY",
 			"threshold": 1
-		}, {"category": "HARM_CATEGORY_VIOLENCE", "threshold": 2}, {
+		}, { "category": "HARM_CATEGORY_VIOLENCE", "threshold": 2 }, {
 			"category": "HARM_CATEGORY_SEXUAL",
 			"threshold": 2
-		}, {"category": "HARM_CATEGORY_MEDICAL", "threshold": 2}, {"category": "HARM_CATEGORY_DANGEROUS", "threshold": 2}],
+		}, { "category": "HARM_CATEGORY_MEDICAL", "threshold": 2 }, { "category": "HARM_CATEGORY_DANGEROUS", "threshold": 2 }],
 		prompt: {
 			text: prompt,
 		},
